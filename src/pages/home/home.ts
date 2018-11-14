fa4571a814ad29a928d3215aed0e5ad50ecaa237
NeshMx <alex.huerta0424@hotmail.com>
2018-11-13 23:14:11 -0600

    pagina de especies casi terminada

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, ModalController } from 'ionic-angular';
 
+import { Item } from '../../models/item';
+import { Items } from '../../providers';
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  currentItems: Item[];
 
+  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController) {
+    this.currentItems = this.items.query();
+  }
+
+  ionViewDidLoad() {
+  }
+
+  /**
+   * Navigate to the detail page for this item.
+   */
+  openItem(item: Item) {
+    this.navCtrl.push('ItemDetailPage', {
+      item: item
+    });
   }
 
 }
---
+++ src/providers/index.ts
@@ -0,0 +1,1 @@
+export { Items } from '../mocks/providers/items';
